e41aaf509a52e415b28d5806a598162e19034224
StarKnightt <prasenjitt4e@gmail.com>
2023-10-09 21:41:11 +0530

    learned about javascript strings

---
+++ All about Learning/JS/JavaScript3.js
@@ -0,0 +1,13 @@
+var String = 'Prasen ,';
+var name = ' Prasenjit ,';
+var message = ' Hello,World!';
+console.log(String);
+console.log(String + name + message);
+var channel = 'CodeWithPrasen';
+var temp = `${name} is a nice person and he has a channel called ${channel}`;
+// console.log(temp);
+// var message = message.length;
+// console.log(`length of the message is ${ message}`);
+// console.log("Hello \ world\nPrasen\tand");
+
+document.getElementById('content').innerHTML = '<h2> This is an h2 heading </h2>'
