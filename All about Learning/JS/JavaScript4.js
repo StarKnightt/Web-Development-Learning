5fe2dc1710c06ef9dea54872dbe799a5321d051e
StarKnightt <prasenjitt4e@gmail.com>
2023-10-10 18:12:22 +0530

    Learned about string methods

---
+++ All about Learning/JS/JavaScript4.js
@@ -0,0 +1,38 @@
+let str = "This is a string";
+console.log(str);
+
+// First Occurence of a susbstring
+let position = str.indexOf('is');
+console.log(position);
+
+// Last Occurence of a susbstring
+position = str.lastIndexOf('is');
+console.log(position);
+
+// Substring from a string
+let hx = str.slice(1,6);
+console.log(hx);
+
+// let hx1 = str.substring(1,3);
+// console.log(hx1);
+
+// let replace = str.replace('string', 'Prasen');
+// console.log(str);
+// console.log(replace);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let newString = str.concat(' Welcome ');
+// console.log(newString);
+
+// let whiteSpaces = "             This string contains White Spaces                ";
+// console.log(whiteSpaces);
+// console.log(whiteSpaces.trim(""));
+
+// let char = str.charAt(2);
+// let chars = str.charCodeAt(2);
+// console.log(chars);
+// console.log(char);
+
+console.log(str[3]);
