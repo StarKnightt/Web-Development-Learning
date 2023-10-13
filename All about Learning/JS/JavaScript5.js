291587a31c6001c3fa65819c0a362781a98c09d7
StarKnightt <prasenjitt4e@gmail.com>
2023-10-14 00:20:14 +0530

    Learned about javascirpt scope,if-else and switch case statement

---
+++ All about Learning/JS/JavaScript5.js
@@ -0,0 +1,54 @@
+// var string1 = "This is a String";
+// console.log(string1);
+// let a = "u";
+// {
+//     let a = "u6";
+//     console.log(a);
+// }
+
+// const a = "This can't be changed";
+// console.log(a);
+
+// let age = 5;
+// if (age>18) {
+//     console.log("You can drink water");
+// }
+// else if(age==2){
+//      console.log("Go and sleep kid");
+// }
+// else if(age==5){
+//      console.log("Go and play kiddo");
+// }
+// else{
+//     console.log("You can drink Cold Drink");
+// }
+
+
+const cup = 45;
+switch (cup) {
+    case 4:
+        console.log("The valuse of cups is 5");
+        break;
+    case 41:
+        console.log("The valuse of cups is 41");
+        break;
+    case 43:
+        console.log("The valuse of cups is 43");
+        break;
+    case 42:
+        console.log("The valuse of cups is 42");
+        break;
+    case 45:
+        console.log("The valuse of cups is 45");
+        break;
+    case 46:
+        console.log("The valuse of cups is 46");
+        break;
+    case 47:
+        console.log("The valuse of cups is 47");
+        break;
+
+    default:
+        console.log("The value of Cup is wrong");
+        break;
+}
