e350410a2d449fb91fe934449ffccadb34e81f6a
StarKnightt <prasenjitt4e@gmail.com>
2023-10-14 21:37:10 +0530

    learned about arrays and array methods

---
+++ All about Learning/JS/JavaScript6.js
@@ -0,0 +1,32 @@
+let myVar = 34;
+let myVar2 = 34;
+let myVar3 = true;
+let myVar4 = null;
+let myVar5 = undefined;
+
+// Object - An object is a collection of data that is stored in name-value pairs. The names are called properties and the values can be anything, such as numbers, strings, arrays, or other objects. Objects are created using curly braces {} and the properties are separated by commas.
+
+// let employee = {
+//     name:"Prasen",
+//     salary: 130,
+//     channel: "Code With Prasen",
+//     channel2: "Programming With Prasen"
+// }
+// console.log(employee);
+
+// let salary = employee.salary;
+// console.log(salary);
+
+// let call = employee["name"];
+// console.log(call);
+
+// Array 
+
+// let names = [1, 2, 4, "Prasen", undefined];
+// let names = new Array(1, 2, 4, "Prasen", undefined);
+let names = new Array(23);
+console.log(names[3]);
+names = names.sort();
+console.log(names.length);
+names.push("Hello");
+console.log(names);
