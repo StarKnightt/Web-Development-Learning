4f65394d28ab5149e9b76cb18e6a1f3609a6e3ca
StarKnightt <prasenjitt4e@gmail.com>
2023-10-16 20:28:33 +0530

    Learned about JavaScript's Interaction - Alert, Prompt, Confirm

---
+++ All about Learning/JS/JavaScript8.js
@@ -0,0 +1,34 @@
+// Alert in in-browser Javascript - doesn't return anything.
+// alert("Reload the page again");
+
+// let a = alert("Reload the page again");
+// console.log(a);
+
+// Prompt in javascript
+
+// let name = prompt("Hello ? ", "User");
+// console.log("Hello, " + name + "!");
+
+// Confirm in js
+
+// let deletePost = confirm("Do you really want to delete this post ?");
+// console.log(deletePost);
+
+// if (deletePost) {
+    // Code to delete the post
+    // console.log("Your post has been deleted successfully");
+// }
+// else{
+    // Code to cancel deletion of post
+    // console.log("Your post has not deleted successfully");
+// }
+
+let ages = prompt(" What is your age Munna?");
+
+if (ages>=18) {
+    console.log (" You are allowed in club, Apka swagat hai club ko");
+}
+else{
+    console.log(" You're not allowed to club, Gande bachhe ");
+}
+
