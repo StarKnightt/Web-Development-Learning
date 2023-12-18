4352fdb681dcda6074ce73ac976722ea30708e0d
StarKnightt <prasenjitt4e@gmail.com>
2023-12-18 23:20:27 +0530

    added

--- All-About-Learning/Node-JS/mod.js
+++ All-About-Learning/Node-JS/mod.js
@@ -3,15 +3,12 @@ console.log("this is Module");
 function average(arr) {
     let sum = 0;
     arr.forEach(element => {
-        sum += element;
     });
     return sum / arr.length;
 }
-
 // module.exports = {  // This is object 
 //     avg: average,
 //     name: "Prasen",
 //     repo: "GitHub"
 // } 
-
 module.exports.name = "Prasen";
