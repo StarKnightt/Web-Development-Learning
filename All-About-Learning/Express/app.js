69f92e2f3cd4d12f23e5680751ef4678fa6fe499
StarKnightt <prasenjitt4e@gmail.com>
2023-12-29 23:15:56 +0530

    learned about simple express routing

--- All-About-Learning/Express/app.js
+++ All-About-Learning/Express/app.js
@@ -11,12 +11,29 @@ app.get("/",(req, res)=>{  // get request in '/' endpoint
     res.send("this is my homepage with first express app writing with Harish bhaiaya");
 });
 
+// To send the status code
+app.get("/",(req, res)=>{  // get request in '/' endpoint
+    res.status(200).send("this is my homepage with first express app writing with Harish bhaiaya");
+});
+
 app.get("/about", (req, res)=>{  // in get method, want to get request in '/about' endpoint
     res.send("This is about page my first express app with Harish");
 });
 
 app.post("/about", (req, res)=>{ // in post method, want to get request in '/about' endpoint
     res.send("This is a post request about page my first express app with Harish");
+
+});
+app.get("/this", (req, res)=>{  // to display page not found
+    res.status(404).send("This page is not found");
+});
+
+app.post("/hello", (req, res)=>{ // To display no content
+    res.status(204).send("No content are available here");
+});
+
+app.get("/display", (req, res)=>{ // To display no content
+    res.status(201).send("The request was successfull, a new resource created");
 });
 
 app.listen(port, ()=>{  // to listen the app on a port
