13fc0f6e6b8f3055737cfc93e346be2865401701
StarKnightt <prasenjitt4e@gmail.com>
2024-01-06 14:22:27 +0530

    Learned about middlewire on express.js as well as how to store the data in the wordfile

--- All-About-Learning/Express/app1.js
+++ All-About-Learning/Express/app1.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 80;
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')); // for serving static files
 app.get('/static',(req,res)=>{
-    res.status(200).render('style.css');
+    res.status(200).sendFile('style.css');
 })
+
+app.use(express.urlencoded({extended:true}))
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug'); // set the template engine as pug
@@ -20,10 +23,23 @@ app.get('/', (req, res) => {
     res.status(200).render('index',param);
 })
 
-app.post('/', (req,res)=>{
-    const param = { 'message': 'yes, its working' }; 
-    res.status(200).render('index',param);
-})
+app.post('/', (req, res) => {
+    // Extracting data from the request body
+    const { name, age, address, more } = req.body;
+
+    // Creating a string to write to the file
+    let outputToWrite = `The name of the client is ${name}, 
+    ${age} years old, 
+    residing at ${address}, 
+    More about him/her: ${more}`;
+
+    // Writing data to the file 'output.txt'
+    fs.writeFileSync('output.txt', outputToWrite);
+
+    // Rendering a response to the client
+    const param = { 'message': 'yes, its working' };
+    res.status(200).render('index', param);
+});
 
 app.listen(port, () => { // to listen the app on a port
     console.log(`The application started running successfully on the port ${port}`);
