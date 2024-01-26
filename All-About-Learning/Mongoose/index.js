2aec8d4cba43847ff90995c62b29734ca29dc5b6
StarKnight <prasenjitt4e@gmail.com>
2024-01-26 23:29:16 +0530

    started designing a cursor

--- All-About-Learning/Mongoose/index.js
+++ All-About-Learning/Mongoose/index.js
@@ -1,23 +1,39 @@
 import mongoose from "mongoose";
-import  express  from "express";
+import express from "express";
 import { Todo } from "./Model/Todo.js";
 
-let conn = await mongoose.connect("mongodb://localhost:27017/todo")
-const app = express()
+// Connect to MongoDB
+let conn = await mongoose.connect("mongodb://localhost:27017/todo");
+
+// Create an Express app
+const app = express();
 const port = 3000;
 
+// Define a route for the root URL
 app.get('/', (req, res) => {
-    const todo = new Todo({title: "Hey first todo", desc: "Description of this Todo", 
-    isDone: "false",days:Math.floor(Math.random()*45 + 5*Math.random())})
-    todo.save()
-    res.send('Hello World!')
-})
-// app.get('/a', async (req, res) => {
-//    let todo = await Todo.findOne({})
-//    console.log("todo is running");
-//    res.json({title: todo.title, desc: todo.desc})
-// })
+    // Create a new Todo and save it to the database
+    const todo = new Todo({
+        title: "Hey first todo",
+        desc: "Description of this Todo",
+        isDone: "false",
+        days: Math.floor(Math.random() * 45 + 5 * Math.random())
+    });
+    todo.save();
+    res.send('Hello World!');
+});
 
+/*
+// Example route that retrieves a Todo from the database
+app.get('/a', async (req, res) => {
+    // Find one Todo from the database
+    let todo = await Todo.findOne({});
+    console.log("todo is running");
+    // Send JSON response with Todo details
+    res.json({ title: todo.title, desc: todo.desc });
+});
+*/
+
+// Start the Express app
 app.listen(port, () => {
-  console.log(`Your server is running successfully ${port}`)
-})
+    console.log(`Your server is running successfully ${port}`);
+});
